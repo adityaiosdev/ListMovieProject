efddb299c6ea2f7567500e93f0d45f6726833103
Aditya Ramadhan <eyditye@gmail.com>
2022-11-15 10:25:20 +0700

    Add project to git

---
+++ ListMovieProject/ListMovieProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListMovieProjectApp.swift
+//  ListMovieProject
+//
+//  Created by Aditya Ramadhan on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ListMovieProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
